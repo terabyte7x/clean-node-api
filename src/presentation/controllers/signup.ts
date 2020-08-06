1ed4f98806789e104cebaa83827884168a6a03a4
Felipe Duque <felipe@duque.dev>
2020-08-06 09:08:09 -0300

    refactor: move server error to a helper method

--- src/presentation/controllers/signup.ts
+++ src/presentation/controllers/signup.ts
@@ -1,7 +1,7 @@
 /* eslint-disable no-unused-vars */
 import { IHttpRequest, IHttpResponse } from '../protocols/IHttp'
 import { MissingParamError } from '../errors/missing-param-error'
-import { badRequest } from '../helpers/http-helper'
+import { badRequest, serverError } from '../helpers/http-helper'
 import { IController } from '../protocols/IController'
 import { IEmailValidator } from '../protocols/IEmailValidator'
 import { InvalidParamError } from '../errors/invalid-param-error'
@@ -28,10 +28,7 @@ export class SignUpController implements IController {
         return badRequest(new InvalidParamError('email'))
       }
     } catch (error) {
-      return {
-        statusCode: 500,
-        body: new ServerError()
-      }
+      return serverError()
     }
   }
 }
--- src/presentation/helpers/http-helper.ts
+++ src/presentation/helpers/http-helper.ts
@@ -1,7 +1,13 @@
 // eslint-disable-next-line no-unused-vars
-import { HttpResponse } from '../protocols/http'
+import { IHttpResponse } from '../protocols/IHttp'
+import { ServerError } from '../errors/server-error'
 
-export const badRequest = (error: Error): HttpResponse => ({
+export const badRequest = (error: Error): IHttpResponse => ({
   statusCode: 400,
   body: error
+})
+
+export const serverError = (): IHttpResponse => ({
+  statusCode: 500,
+  body: new ServerError()
 })
