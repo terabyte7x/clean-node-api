40fab50b45343b1c72386ec5e6df3a5a504ca4e0
Felipe Duque <felipe@duque.dev>
2020-08-06 08:42:54 -0300

    feat: create constructor to receive emailValidator interface and validate if the email is valid

--- src/presentation/controllers/signup.ts
+++ src/presentation/controllers/signup.ts
@@ -1,23 +1,29 @@
-// eslint-disable-next-line no-unused-vars
-import { HttpRequest, HttpResponse } from '../protocols/http'
+/* eslint-disable no-unused-vars */
+import { IHttpRequest, IHttpResponse } from '../protocols/IHttp'
 import { MissingParamError } from '../errors/missing-param-error'
 import { badRequest } from '../helpers/http-helper'
+import { IController } from '../protocols/IController'
+import { IEmailValidator } from '../protocols/IEmailValidator'
+import { InvalidParamError } from '../errors/invalid-param-error'
 
-export class SignUpController {
-  handle (httpRequest: HttpRequest):HttpResponse {
-    // if (!httpRequest.body.name) {
-    //   return badRequest(new MissingParamError('name'))
-    // }
-    // if (!httpRequest.body.email) {
-    //   return badRequest(new MissingParamError('email'))
-    // }
+export class SignUpController implements IController {
+  private readonly emailValidator: IEmailValidator
 
+  constructor (emailValidator: IEmailValidator) {
+    this.emailValidator = emailValidator
+  }
+
+  handle (httpRequest: IHttpRequest):IHttpResponse {
     const requiredFields = ['name', 'email', 'password', 'passwordConfirm']
 
     for (const field of requiredFields) {
       if (!httpRequest.body[field]) {
         return badRequest(new MissingParamError(field))
       }
+    }
+    const isValid = this.emailValidator.isValid(httpRequest.body.email)
+    if (!isValid) {
+      return badRequest(new InvalidParamError('email'))
     }
   }
 }
